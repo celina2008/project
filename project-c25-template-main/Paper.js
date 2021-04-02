6eb8ec10a280fc256edd39a04ce7a2e7d9b2b0e0
celina2008 <76843304+celina2008@users.noreply.github.com>
2021-04-02 23:13:23 +0530

    Add files via upload

---
+++ project-c25-template-main/Paper.js
@@ -0,0 +1,18 @@
+class Paper{
+
+
+
+    constructor(x, y, width, height){
+
+    
+    var options = {
+        isStatic:false,
+    restitution :0.3,
+    density:1.2,
+    friction:0,
+
+    }
+}
+    
+
+}
